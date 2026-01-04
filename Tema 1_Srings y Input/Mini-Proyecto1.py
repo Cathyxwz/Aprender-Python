7476d218264412a99d9a90be62081c4a73778f06
Cathy <112209690+Cathyxwz@users.noreply.github.com>
2026-01-03 21:54:43 -0500

    El mini proyecto usa input() para pedir dos palabras al usuario y print() para mostrarlas juntas como una marca creativa.

---
+++ Tema 1_Srings y Input/Mini-Proyecto1.py
@@ -0,0 +1,7 @@
+# El ejercicio pide crear un código que haga dos preguntas de una sola palabra cada una
+# Luego debe mostrar esas respuestas combinadas en pantalla como una marca creativa
+
+# Se usa print para mostrar el resultado final
+# Se usan dos input() para pedir las palabras al usuario
+# Se concatenan las respuestas con un espacio en medio
+print("El nombre de su cerveza es: " + input("¿Cúales son sus animales preferidos?") + " " + input("¿Que climas le gusta más, frios o calurosos?"))
