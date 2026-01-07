352a4c4225fe8bb1ef0078a1bb013afd240f74e1
Cathy <112209690+Cathyxwz@users.noreply.github.com>
2026-01-06 21:54:08 -0500

    Aprendí a identificar tipos de datos en Python usando type(), trabajando con enteros (int) y decimales (float).

---
+++ Tema 2_Tipo de datos y Variables/Practicando-Int y Float.py
@@ -0,0 +1,10 @@
+# Se crea una variable entera y se muestra su valor y tipo de dato
+mi_numero = 1
+print(mi_numero)
+print(type(mi_numero))
+
+# Se crea una variable decimal (float) y se muestra su valor y tipo de dato
+mi_numero2 = 5.8
+print(mi_numero2)
+print(type(mi_numero2))
+
